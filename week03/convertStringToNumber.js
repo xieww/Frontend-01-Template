68089b31b0527454a05cef346d1d590559da80e2
xieww <2801416900@qq.com>
2020-04-27 23:08:26 +0800

    fix(week03): 字符串和数字的互相转换

--- week03/convertStringToNumber.js
+++ week03/convertStringToNumber.js
@@ -1,52 +1,84 @@
-function convertStringToNumber(str, hex) {
-  if (arguments.length < 2) {
-    hex = 10;
-  }
-  const arr = str.split('');
-  var num = 0;
+
+/**
+ * 默认转换进制为10，当没有输入转换进制默认按照10进制
+ * @param {*} str
+ * @param {*} hex
+ * @returns
+ */
+function convertStringToNumber(str, hex = 10) {
+  // 处理转换进制是否在2-36之间
+  if (typeof hex !== 'number' || hex < 2 || hex > 36) {
+    return NaN;
+  };
+
+  let num = 0;
   let i = 0;
 
-  while (i < arr.length && arr[i] !== '.') {
-    const element = arr[i];
-    num = num * hex;
-    num += element.codePointAt(0) - '0'.codePointAt(0);
-    i++;
-  }
+  // 这里要判断str亦即没有传入任何值时应返回0
+  if (str) {
+    const arr = String(str).split(''); // 防止要转换的值不是字符串
+    while (i < arr.length && arr[i] !== '.') {
+      const element = arr[i];
+      num = num * hex;
+      num += element.codePointAt(0) - '0'.codePointAt(0);
+      i++;
+    }
 
-  if (arr[i] === '.') {
-    i++;
-  }
+    if (arr[i] === '.') {
+      i++;
+    }
 
-  let decimal = 1;
-  while (i < arr.length) {
-    const element = arr[i];
-    decimal = decimal / hex;
-    num += (element.codePointAt(0) - '0'.codePointAt(0)) * decimal;
-    i++;
+    let decimal = 1;
+    while (i < arr.length) {
+      const element = arr[i];
+      decimal = decimal / hex;
+      num += (element.codePointAt(0) - '0'.codePointAt(0)) * decimal;
+      i++;
+    }
   }
 
   return num;
 }
 console.log(convertStringToNumber("10.0123"));
 
-function convertNumberToString(num, hex) {
-  if (arguments.length < 2) {
-    hex = 10;
-  }
+function convertNumberToString(num, hex = 10) {
+  // 处理转换进制是否在2-36之间
+  if (typeof hex !== 'number' || hex < 2 || hex > 36) {
+    return Error('radix argument must be between 2 and 36');
+  };
 
-  if (num === 0) {
-    str = num + ''; // 或者 `${num}`
-    return str;
+  // 处理为输入或者输入为空的情况
+  if (arguments.length === 0 || num === '') {
+    return '""';
   }
-  var integer = Math.floor(num);// 整数部分
-  var decimal = num - integer;// 小数部分
-  console.log('decimal', decimal);
-
   var str = '';
-  while (integer > 0) {
-    str = String(integer % hex) + str;
-    integer = Math.floor(integer / hex);
+  // 判断要转换的值是否为number类型
+  if (typeof num === 'number') {
+    // 当要转换的值为0时
+    if (num === 0) {
+      str = num + ''; // 或者 `${num}`
+      return str;
+    } else {
+      var integer = Math.floor(num);// 整数部分
+      var decimal = num - integer;// 小数部分
+      while (integer > 0) {
+        str = String(integer % hex) + str;
+        integer = Math.floor(integer / hex);
+      }
+
+      var t = String(decimal).substr(1);
+      
+      str = str + t;
+      console.log('decimal', decimal, str, t);
+
+    }
+  } else {
+    str = num;
   }
   return str;
 }
 console.log(convertNumberToString(0));
+console.log(convertNumberToString(undefined));
+console.log(convertNumberToString(null));
+console.log(convertNumberToString(123.089));
+console.log(convertNumberToString('456'));

0914fad25a129be857c91637322f748cb4213ab0
xieww <2801416900@qq.com>
2020-04-28 23:41:45 +0800

    feat(weeko3): week03

--- week03/NOTE.md
+++ week03/NOTE.md
@@ -1,1 +1,39 @@
 # 每周总结可以写在这里
+
+## JavaScript 中的特殊对象
+
+- Bound Function Exotic Objects
+
+```js
+绑定函数是包装另一个函数对象的外来对象。绑定函数是可调用的(它有一个[[Call]]内部方法，可以有一个[[Construct]]内部方法)。调用绑定函数通常会导致调用其包装函数。
+
+[[BoundTargetFunction]] 被 bind 的函数
+[[BoundThis]] bind 时候的 this 参数
+[[BoundArguments]] bind 时候的其他参数
+
+```
+
+- Array Exotic Objects 数组对象
+
+```js
+```
+
+- String Exotic Objects 字符串对象
+
+- Arguments Exotic Objects 参数对象
+
+- Integer-Indexed Exotic Objects
+
+- Module Namespace Exotic Objects
+
+- Module Namespace Exotic Objects
+
+```js
+[[Module]]   Module Record   模块记录其导出的名称空间。
+[[Exports]]  List of String   包含作为该对象的自身属性公开的导出名称的字符串值的列表。该列表的排序方式与使用Array.prototype对这些字符串值的数组排序一样。使用undefined作为comparefn排序
+[[Prototype]] null   这个槽总是包含空值
+```
+
+- Immutable Prototype Exotic Objects
+
+- Proxy Object Internal Methods and Internal Slots
--- week03/convertStringToNumber.js
+++ week03/convertStringToNumber.js
@@ -1,30 +1,30 @@
-
 /**
  * 默认转换进制为10，当没有输入转换进制默认按照10进制
+ * 目前转换的字符首位不能包含“-”
  * @param {*} str
  * @param {*} hex
  * @returns
  */
 function convertStringToNumber(str, hex = 10) {
   // 处理转换进制是否在2-36之间
-  if (typeof hex !== 'number' || hex < 2 || hex > 36) {
+  if (typeof hex !== "number" || hex < 2 || hex > 36) {
     return NaN;
-  };
+  }
 
   let num = 0;
   let i = 0;
 
   // 这里要判断str亦即没有传入任何值时应返回0
   if (str) {
-    const arr = String(str).split(''); // 防止要转换的值不是字符串
-    while (i < arr.length && arr[i] !== '.') {
+    const arr = String(str).split(""); // 防止要转换的值不是字符串
+    while (i < arr.length && arr[i] !== ".") {
       const element = arr[i];
       num = num * hex;
-      num += element.codePointAt(0) - '0'.codePointAt(0);
+      num += element.codePointAt(0) - "0".codePointAt(0);
       i++;
     }
 
-    if (arr[i] === '.') {
+    if (arr[i] === ".") {
       i++;
     }
 
@@ -32,45 +32,41 @@ function convertStringToNumber(str, hex = 10) {
     while (i < arr.length) {
       const element = arr[i];
       decimal = decimal / hex;
-      num += (element.codePointAt(0) - '0'.codePointAt(0)) * decimal;
+      num += (element.codePointAt(0) - "0".codePointAt(0)) * decimal;
       i++;
     }
   }
 
   return num;
 }
-console.log(convertStringToNumber("10.0123"));
+console.log(convertStringToNumber("1024.0128"));
 
 function convertNumberToString(num, hex = 10) {
   // 处理转换进制是否在2-36之间
-  if (typeof hex !== 'number' || hex < 2 || hex > 36) {
-    return Error('radix argument must be between 2 and 36');
-  };
+  if (typeof hex !== "number" || hex < 2 || hex > 36) {
+    throw new Error("radix argument must be between 2 and 36");
+  }
 
   // 处理为输入或者输入为空的情况
-  if (arguments.length === 0 || num === '') {
+  if (arguments.length === 0 || num === "") {
     return '""';
   }
-  var str = '';
+  var str = "";
   // 判断要转换的值是否为number类型
-  if (typeof num === 'number') {
+  if (typeof num === "number") {
     // 当要转换的值为0时
     if (num === 0) {
-      str = num + ''; // 或者 `${num}`
+      str = num + ""; // 或者 `${num}`
       return str;
     } else {
-      var integer = Math.floor(num);// 整数部分
-      var decimal = num - integer;// 小数部分
+      var integer = Math.floor(num); // 整数部分
+      var decimal = num - integer; // 小数部分
       while (integer > 0) {
         str = String(integer % hex) + str;
         integer = Math.floor(integer / hex);
       }
-
       var t = String(decimal).substr(1);
-      
       str = str + t;
-      console.log('decimal', decimal, str, t);
-
     }
   } else {
     str = num;
@@ -81,4 +77,5 @@ console.log(convertNumberToString(0));
 console.log(convertNumberToString(undefined));
 console.log(convertNumberToString(null));
 console.log(convertNumberToString(123.089));
-console.log(convertNumberToString('456'));
+console.log(convertNumberToString("456"));
+console.log(convertNumberToString(12348));

536b3473e2001b7e43ad77ca31b8ed79ca9587e0
xieww <2801416900@qq.com>
2020-06-08 23:34:37 +0800

    feat(week): week

--- week09/index.js
+++ week09/index.js
@@ -280,14 +280,3 @@ const list = [
   "caret-color",
   "line-break",
 ];
-
-// var isPowerOfTwo = function(n) {
-//   if (n <= 0 ) {
-//     return false;
-//   }
-
-//   while (n % 2 === 0) {
-//     n /= 2;
-//   }
-//   return n === 1;
-// };
